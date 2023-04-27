25eff3e0d2ddc0b1d00a0a9d16a1c6e1d2cd7048
saulwade <saulwade29@gmail.com>
2023-04-26 19:45:33 -0600

    added some code

---
+++ controllers/userController.js
@@ -0,0 +1,46 @@
+const User = require('../models/user');
+
+const userController = {
+  // Get all users
+  getAllUsers(req, res) {
+    User.find({})
+      .then((dbUserData) => res.json(dbUserData))
+      .catch((err) => res.status(400).json(err));
+  },
+
+  // Get a single user by id
+  getUserById(req, res) {
+    User.findOne({ _id: req.params.id })
+      .populate({ path: 'thoughts', select: '-__v' })
+      .populate({ path: 'friends', select: '-__v' })
+      .select('-__v')
+      .then((dbUserData) => res.json(dbUserData))
+      .catch((err) => res.status(400).json(err));
+  },
+
+  // Create a new user
+  createUser(req, res) {
+    User.create(req.body)
+      .then((dbUserData) => res.json(dbUserData))
+      .catch((err) => res.status(400).json(err));
+  },
+
+  // Update a user by id
+  updateUser(req, res) {
+    User.findOneAndUpdate({ _id: req.params.id }, req.body, {
+      new: true,
+      runValidators: true,
+    })
+      .then((dbUserData) => res.json(dbUserData))
+      .catch((err) => res.status(400).json(err));
+  },
+
+  // Delete a user by id
+  deleteUser(req, res) {
+    User.findOneAndDelete({ _id: req.params.id })
+      .then((dbUserData) => res.json(dbUserData))
+      .catch((err) => res.status(400).json(err));
+  },
+};
+
+module.exports = userController;
